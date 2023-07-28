936050d43f0ac984b7ce3e1738c0fd2e5270380c
Ashwini Chawla <chawlaas@oregonstate.edu>
2023-07-27 18:32:59 -0700

    added test for passwords greater than 20 characters

--- tests.py
+++ tests.py
@@ -6,6 +6,10 @@ class TestCase(unittest.TestCase):
     def test1(self):
         """Verify that passwords less than 8 characters are rejected"""
         self.assertFalse(check_pwd('abcdabc'))
+        
+    def test2(self):
+        """Verify that passwords greater than 20 characters are rejected"""
+        self.assertFalse(check_pwd('abcdabcdabcdabcdabcda'))
 
 if __name__ == '__main__':
     unittest.main()
