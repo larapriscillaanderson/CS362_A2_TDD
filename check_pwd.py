845001c84f20689385da6299501dbfb0ec495085
Ashwini Chawla <chawlaas@oregonstate.edu>
2023-07-27 18:42:55 -0700

    updated check_pwd to ensure it contains a lower case letter

--- check_pwd.py
+++ check_pwd.py
@@ -1,5 +1,13 @@
+import re
+
 def check_pwd(pwd):
+    # validate length
     if len(pwd) < 8 or len(pwd) > 20:
+        return False
+    
+    # check for a lowercase letter
+    lower_case = re.search('[a-z]', pwd)
+    if lower_case is None:
         return False
     
     return True
